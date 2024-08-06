e0f1bdf244208d215565e2f81c1b201566e70cf7
NemanjaMaks97 <maksimovic.nemanja.997@gmail.com>
2024-08-06 21:48:44 +0200

    Spawning obstacles
    Spawn random obstacle at random intervals

---
+++ Assets/GameController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public List<GameObject> Obstacles;
+
+    public Transform SpawnerTop;
+    public Transform SpawnerBottom;
+
+    private Timer timer;
+    private System.Random random;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //SpawnObstacle();
+        random = new System.Random();
+        //timer = new Timer(2000);
+        //timer.Elapsed += Timer_Elapsed;
+        //timer.AutoReset = false;
+        //timer.Enabled = true;
+        //timer.Start();
+        StartCoroutine(SpawnTimer());
+    }
+
+    private IEnumerator SpawnTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(random.Next(1, 2));
+            SpawnObstacle();
+        }
+    }
+
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        SpawnObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnObstacle()
+    {
+        Debug.Log("SpawnObstacle");
+        int spawnIndex = random.Next(0, Obstacles.Count);
+        Debug.Log(spawnIndex);
+        var obj = GameObject.Instantiate(Obstacles[spawnIndex]);
+        obj.SetActive(true);
+        obj.transform.position = new Vector3 (SpawnerTop.position.x, (float)GetNextPosition(), -1);
+    }
+
+    private double GetNextPosition()
+    {
+        return random.NextDouble() * (SpawnerTop.position.y - SpawnerBottom.position.y) + SpawnerBottom.position.y;
+    }
+
+    private void OnApplicationQuit()
+    {
+        StopAllCoroutines();
+    }
+}
--- Assets/ObstacleController.cs
+++ Assets/ObstacleController.cs
@@ -7,22 +7,29 @@ public class ObstacleController : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    public List<GameObject> Obstacles;
+    private void OnEnable()
+    {
+        //gameObject.transform.position = Vector3.zero;
+        Rigidbody rigidbody = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+        rigidbody.AddForce(Vector3.left * speed);
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var obj = Instantiate(Obstacles[0]);
-        obj.transform.position = Vector3.zero;
-        obj.gameObject.SetActive(true);
-        Rigidbody rigidbody = obj.GetComponent(typeof(Rigidbody)) as Rigidbody;
-        rigidbody.AddForce(Vector3.left * speed);
+        //var obj = Instantiate(Obstacles[0]);
+        //obj.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 
 }
